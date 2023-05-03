597d78466fe7e33dee55c5d4e7da19957d56101d
BelousovaAnna215 <113383599+BelousovaAnna215@users.noreply.github.com>
2023-05-03 10:17:10 +0300

    Update dairyfarm.cpp

--- dairyfarm.cpp
+++ dairyfarm.cpp
@@ -1,12 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include <list>
+#include <string.h>
+
 using namespace std;
 
 class Animal 
 { 
    string name;
+   char animal_gender;
 public:
 	Animal(){
-		cout<<"animal constructor"<<endl;
+		//cout<<"animal constructor"<<endl;
+	}
+	void gender(){
+		cout<<"What gender? (f/m)"<<endl;
+		cin>>animal_gender;
+	}
+	bool gender_is(){
+		cout<<animal_gender<<endl;
+		 if (animal_gender == 'f')
+			return true;
+		else return false;
 	}
     virtual void makesound(){
         cout<<"nothing"<<endl;
@@ -17,18 +31,19 @@ public:
     virtual void getmilk(){
         cout<<"from who?"<<endl;
     }
-    ~Animal(){
-		cout<<"animal destructor"<<endl;
+    virtual ~Animal(){
+		//cout<<"animal destructor"<<endl;
 	}
 };
+
 
 class Cow : public Animal
 { 
 public:
 	Cow() {
-        cout<<"you have one more cow"<<endl;
+        //cout<<"you have one more cow"<<endl;
     }
-    virtual void makesound() {
+    void makesound() {
         cout<<"mooooo"<<endl;
     }
     virtual void graze(){
@@ -38,21 +53,21 @@ public:
 		cout<<"have cow milk"<<endl;
 	}
 	~Cow() {
-        cout<<"one less cow"<<endl;
+        //cout<<"one less cow"<<endl;
     }
 };
 
 class Goat : public Animal
 { 
 public:
-    virtual void makesound(){
+    void makesound(){
         cout<<"baaaaa"<<endl;
     }
     Goat() {
-        cout<<"you have one more goat"<<endl;
+        //cout<<"you have one more goat"<<endl;
     }
     ~Goat() {
-        cout<<"one less goat"<<endl;
+        //cout<<"one less goat"<<endl;
     }
     virtual void graze(){
        cout<<"goat eat"<<endl;
@@ -62,21 +77,46 @@ public:
    } 
 };
 
+class Sheep : public Animal
+{ 
+public:
+    void makesound(){
+        cout<<"baaaaa"<<endl;
+    }
+    Sheep() {
+        //cout<<"you have one more sheep"<<endl;
+    }
+    ~Sheep() {
+        //cout<<"one less sheep"<<endl;
+    }
+    virtual void graze(){
+       cout<<"sheep eat"<<endl;
+    }
+    virtual void getmilk(){
+       cout<<"have sheep milk"<<endl;
+   } 
+};
+
 
 class Container 
 { 
     int volume;
     int maxvolume = 100;
     bool full;
-    enum type_con {bottle, can, carton};
-    type_con type;
+    string kind;
  public:
     Container(){
-        cout<<"new container"<<endl;
+        //cout<<"new container"<<endl;
     }
     ~Container(){
-        cout<<"~container"<<endl;
+        //cout<<"~container"<<endl;
     }
+    void kind_is(string s){
+	    kind = s;
+    }
+    string kind_of(){
+		return kind;
+	}
     void changev(){
 		cout<<"what is valume?"<<endl;
 	    cin>>volume;
@@ -102,47 +142,212 @@ class Container
 			cout<<"full"<<endl;
 		else cout<<"not full"<<endl;
     }
-    void look(){
-		cout<<"volume is "<<volume<<endl;
+    int look(){
+		return volume;
+	}
+	//из молока сделать кефир
+	void mkkefir(){
+		cout<<"made kefir from milk"<<endl;
+	}
+	// из молока сделать сыр
+	void mkcheese(){
+		cout<<"made cheese from milk"<<endl;
 	}
 };
 
-
-class Products 
-{ 
-public:
-   Products(){
-       cout<<"new products"<<endl;
-   }
-   ~Products(){
-       cout<<"~products"<<endl;
-   }
-    
-};
-
-class Milk : public Products
-{ 
-	double fatcon;
-    
-};
-
+void menu(){
+	cout<<"\n__________________________________________"<<endl;
+	cout<<" 1 - Купить животное "<<endl;
+	cout<<" 2 - Продать животное "<<endl;
+	cout<<" 3 - Звук "<<endl;
+	cout<<" 4 - Покормить "<<endl;
+	cout<<" 5 - Подоить "<<endl;
+	cout<<" 6 - Посмотреть кто есть на ферме "<<endl;
+	
+	cout<<" 7 - Продать продукты "<<endl;
+	cout<<" 9 - Посмотреть сколько молока "<<endl;
+	
+	cout<<" 0 - Пропустить "<<endl;
+	cout<<" 1000 - Выход "<<endl;
+	return;
+}
 
 
 int main(){
-    Cow a;
-    Animal a1;
-    a.makesound();
-    a1.graze();
-    a.getmilk();
+	
+	list<Cow> listCow;
+	list<Goat> listGoat;
+	list<Sheep> listSheep;
+	list<Container> listCon;
+    int i, j;
+    char str[10];
     
-    Container c;
-    c.sell();
-    c.changev();
-    c.isfull();
-    c.makefull();
-    c.look();
-    c.isfull();
-    c.makeempty();
-    c.look();
-    c.isfull();
+    while(1){
+		
+		menu();
+		cin>>i;
+		
+		switch(i){
+         case 1:// купить
+			cout << "What animal do you want to buy?" << endl;
+			cin>>str;
+			if (strcmp(str, "cow") == 0){
+				Cow a;
+				cout<<"you have one more cow"<<endl;
+				a.gender();
+				listCow.push_back(a);
+			} else if (strcmp(str, "goat") == 0){
+				Goat a;
+				cout<<"you have one more goat"<<endl;
+				a.gender();
+				listGoat.push_back(a);
+			} else if (strcmp(str, "sheep") == 0){
+				Sheep a;
+				cout<<"you have one more sheep"<<endl;
+				a.gender();
+				listSheep.push_back(a);
+			}
+            break;
+            
+         case 2:// продать
+			cout << "What animal do you want to sell?" << endl;
+            cin>>str;
+			if (strcmp(str, "cow") == 0){
+				if (listCow.empty())
+					cout << "Have no cow" << endl;
+				else {
+					cout << "Cow sold" << endl;
+					listCow.pop_back();
+				}
+			} else if (strcmp(str, "goat") == 0){
+				if (listGoat.empty())
+					cout << "Have no goat" << endl;
+				else {
+					cout << "Goat sold" << endl;
+					listGoat.pop_back();
+				}
+			} else if (strcmp(str, "sheep") == 0){
+				if (listSheep.empty())
+					cout << "Have no sheep" << endl;
+				else {
+					cout << "Sheep sold" << endl;
+					listSheep.pop_back();
+				}
+			}
+            break;
+            
+         case 3://voice
+            for (Cow n : listCow){
+				n.makesound();
+			}
+			for (Goat n : listGoat){
+				n.makesound();
+			}
+			for (Sheep n : listSheep){
+				n.makesound();
+			}
+            break;
+            
+         case 4://покормить
+            for (Cow n : listCow){
+				n.graze();
+			}
+			for (Goat n : listGoat){
+				n.graze();
+			}
+			for (Sheep n : listSheep){
+				n.graze();
+			}
+            break;
+            
+         case 5://подоить
+            for (Cow n : listCow){
+				if(n.gender_is()){
+					n.getmilk();
+					Container c;
+					c.changev();
+					c.kind_is("cow");
+					listCon.push_back(c);
+				}
+			}
+			for (Goat n : listGoat){
+				if(n.gender_is()){
+					n.getmilk();
+					Container c;
+					c.changev();
+					c.kind_is("goat");
+					listCon.push_back(c);
+				}
+			}
+			for (Sheep n : listSheep){
+				if(n.gender_is()){
+					n.getmilk();
+					Container c;
+					c.changev();
+					c.kind_is("sheep");
+					listCon.push_back(c);
+				}
+			}
+			if (listSheep.empty() and listGoat.empty() and listCow.empty() ){
+				cout<< "Have no animal"<<endl;
+			}
+            break;
+            
+         case 6:
+            cout<<"There are " <<listCow.size()<< " cows on the farm"<< endl;
+            cout<<"There are " <<listGoat.size()<< " goats on the farm"<< endl;
+            cout<<"There are " <<listSheep.size()<< " sheep on the farm"<< endl;
+            break;
+            
+         case 7:
+			cout << "What do you want to sell?" << endl;
+			cout<<" 1 - milk\n 2 - kefir\n 3 - cheese"<<endl;
+            cin>>j;
+			if (j == 1){//milk
+				if (!listCon.empty()){
+					Container c = listCon.back();
+					listCon.pop_back();
+					cout<<"We sell "<<c.kind_of()<<" milk, volume is "<<c.look()<<endl;
+					c.sell();
+				} else 
+					cout<< "We haven't got milk"<<endl;
+				
+			} else if (j == 2){// kefir
+				if (!listCon.empty()){
+					Container c = listCon.back();
+					listCon.pop_back();
+					c.mkkefir();
+					c.sell();
+				} else 
+					cout<< "We haven't got milk to make kefir"<<endl;
+				
+				
+			} else if (j == 3){//cheese
+				if (!listCon.empty()){
+					Container c = listCon.back();
+					listCon.pop_back();
+					c.mkcheese();
+					c.sell();
+				} else 
+					cout<< "We haven't got milk to make cheese"<<endl;
+			}
+            break;
+            
+            
+         case 9:
+            cout<<"There are " <<listCon.size()<< " conteiners on milk"<< endl;
+            break;
+            
+         case 0:
+            break;
+            
+         case 1000:
+            exit(0);
+            
+         default:
+           cout<<" no such command "<<endl;
+           break; 
+      }
+	};
 }
+
